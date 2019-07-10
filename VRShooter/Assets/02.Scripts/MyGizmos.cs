77d65b132e3f8ccd7776afe5753595a1b68293b2
IndieGameMaker <myleje@gmail.com>
2019-07-10 14:33:27 +0900

    .

---
+++ VRShooter/Assets/02.Scripts/MyGizmos.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class MyGizmos : MonoBehaviour
+{
+    public Color _color = Color.yellow;
+    public float _radius = 0.3f;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = _color;
+        Gizmos.DrawSphere(transform.position, _radius);
+    }
+}
