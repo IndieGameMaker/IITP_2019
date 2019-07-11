bd57c935b9c5650a0d45fcc22db93cf0ce3b1c86
IndieGameMaker <myleje@gmail.com>
2019-07-11 11:36:46 +0900

    .

---
+++ VRShooter/Assets/02.Scripts/PlayerCtrl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    private CharacterController cc;
+    private Transform camTr;
+    public float moveSpeed = 1.5f;
+    
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        camTr = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        cc.SimpleMove(camTr.forward * moveSpeed);
+    }
+}
