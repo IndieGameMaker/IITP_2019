fd879a09f836ac10e3be766ab81a9373c02317c9
IndieGameMaker <myleje@gmail.com>
2019-07-11 11:23:54 +0900

    .

---
+++ VRShooter/Assets/02.Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private Transform[] playerPoints;
+    public GameObject player;
+
+    void Start()
+    {
+        playerPoints = GameObject.Find("PlayerSpawn").GetComponentsInChildren<Transform>();
+        int idx = Random.Range(1, playerPoints.Length); // (1, 7) --> 1,2,3,4,5,6
+        Instantiate(player, playerPoints[idx].position, playerPoints[idx].rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
