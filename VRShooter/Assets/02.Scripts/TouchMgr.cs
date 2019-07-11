891d423638006d6d28303a5b5dfc2eaec657963d
IndieGameMaker <myleje@gmail.com>
2019-07-11 10:23:13 +0900

    .

--- VRShooter/Assets/02.Scripts/TouchMgr.cs
+++ VRShooter/Assets/02.Scripts/TouchMgr.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class TouchMgr : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Ray ray;
+    private RaycastHit hit;
+
+    public Transform laserTr;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //레이 생성
+        ray = new Ray(laserTr.position, laserTr.forward);        
+        if (Physics.Raycast(ray, out hit, 50.0f, 1<<8))
+        {
+
+        }
+        else
+        {
+            
+        }
     }
 }
